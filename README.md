3888054a8bfdbe35c65fc8edd0aac0a4fda9efc6
Shalimov <Shalimich@gmail.com>
2015-11-07 14:39:47 +0300

    added readme, updated jsdoc, and added doc script

--- README.md
+++ README.md
@@ -1,11 +1,202 @@
-# JS-Mediator
+<a name="MediatorDefinition"></a>
+## MediatorDefinition : <code>object</code>
+**Kind**: global namespace  
 
-# Simple implementation of mediator pattern.
+* [MediatorDefinition](#MediatorDefinition) : <code>object</code>
+  * [.EventEmitter](#MediatorDefinition.EventEmitter)
+    * [new EventEmitter()](#new_MediatorDefinition.EventEmitter_new)
+    * [.on(eventName, handler)](#MediatorDefinition.EventEmitter+on)
+    * [.once(eventName, handler)](#MediatorDefinition.EventEmitter+once)
+    * [.off(eventName, |handler|)](#MediatorDefinition.EventEmitter+off)
+    * [.emit(eventName)](#MediatorDefinition.EventEmitter+emit)
+    * [.toString()](#MediatorDefinition.EventEmitter+toString) ⇒ <code>string</code>
+  * [.Mediator](#MediatorDefinition.Mediator)
+    * [new Mediator(eventEmitter, setting)](#new_MediatorDefinition.Mediator_new)
+    * _instance_
+      * [.addComponent(name, registrator)](#MediatorDefinition.Mediator+addComponent)
+      * [.removeComponent(name)](#MediatorDefinition.Mediator+removeComponent) ⇒ <code>boolean</code>
+      * [.hasComponent(name)](#MediatorDefinition.Mediator+hasComponent) ⇒ <code>boolean</code>
+      * [.getComponent(name)](#MediatorDefinition.Mediator+getComponent) ⇒ <code>Object</code>
+      * [.componentsList()](#MediatorDefinition.Mediator+componentsList) ⇒ <code>Array</code>
+      * [.shareResponsibility(mediator)](#MediatorDefinition.Mediator+shareResponsibility)
+      * [.toString()](#MediatorDefinition.Mediator+toString) ⇒ <code>string</code>
+    * _static_
+      * [.EVENTS](#MediatorDefinition.Mediator.EVENTS)
+        * [.ADD](#MediatorDefinition.Mediator.EVENTS.ADD)
+        * [.REMOVE](#MediatorDefinition.Mediator.EVENTS.REMOVE)
 
-- Here you can find simple utilities (#map, #each, #extend).
-- Also -> EventEmitter (#on, #off, #emit, #once)
-- And of course -> Mediator (#addComponent, #removeComponent, #hasComponent, #getComponent, #etc...)
+<a name="MediatorDefinition.EventEmitter"></a>
+### MediatorDefinition.EventEmitter
+**Kind**: static class of <code>[MediatorDefinition](#MediatorDefinition)</code>  
 
-At this moment documentation in progress,
-if you want to understand how it works,
-you should look at the tests.
+* [.EventEmitter](#MediatorDefinition.EventEmitter)
+  * [new EventEmitter()](#new_MediatorDefinition.EventEmitter_new)
+  * [.on(eventName, handler)](#MediatorDefinition.EventEmitter+on)
+  * [.once(eventName, handler)](#MediatorDefinition.EventEmitter+once)
+  * [.off(eventName, |handler|)](#MediatorDefinition.EventEmitter+off)
+  * [.emit(eventName)](#MediatorDefinition.EventEmitter+emit)
+  * [.toString()](#MediatorDefinition.EventEmitter+toString) ⇒ <code>string</code>
+
+<a name="new_MediatorDefinition.EventEmitter_new"></a>
+#### new EventEmitter()
+EventEmitter - provides event-driven system
+
+<a name="MediatorDefinition.EventEmitter+on"></a>
+#### eventEmitter.on(eventName, handler)
+Method provide ability to subscribe on some event by name and react on it by handler
+
+**Kind**: instance method of <code>[EventEmitter](#MediatorDefinition.EventEmitter)</code>  
+
+| Param | Type |
+| --- | --- |
+| eventName | <code>string</code> | 
+| handler | <code>function</code> | 
+
+<a name="MediatorDefinition.EventEmitter+once"></a>
+#### eventEmitter.once(eventName, handler)
+Method allows to subscribe on some event and unsubscribe automatically after event will happen
+
+**Kind**: instance method of <code>[EventEmitter](#MediatorDefinition.EventEmitter)</code>  
+
+| Param | Type |
+| --- | --- |
+| eventName | <code>string</code> | 
+| handler | <code>function</code> | 
+
+<a name="MediatorDefinition.EventEmitter+off"></a>
+#### eventEmitter.off(eventName, |handler|)
+Method allows to remove subscription for specify handler of all event if handler is not defined
+
+**Kind**: instance method of <code>[EventEmitter](#MediatorDefinition.EventEmitter)</code>  
+
+| Param | Type |
+| --- | --- |
+| eventName | <code>string</code> | 
+| |handler| | <code>function</code> | 
+
+<a name="MediatorDefinition.EventEmitter+emit"></a>
+#### eventEmitter.emit(eventName)
+Method allows to trigger all handler which are subscribed on some event and also pass any number of arguments
+
+**Kind**: instance method of <code>[EventEmitter](#MediatorDefinition.EventEmitter)</code>  
+
+| Param |
+| --- |
+| eventName | 
+
+<a name="MediatorDefinition.EventEmitter+toString"></a>
+#### eventEmitter.toString() ⇒ <code>string</code>
+Overriding of standart toString method
+
+**Kind**: instance method of <code>[EventEmitter](#MediatorDefinition.EventEmitter)</code>  
+**Returns**: <code>string</code> - Returns string '[object EventEmitter]'  
+<a name="MediatorDefinition.Mediator"></a>
+### MediatorDefinition.Mediator
+**Kind**: static class of <code>[MediatorDefinition](#MediatorDefinition)</code>  
+
+* [.Mediator](#MediatorDefinition.Mediator)
+  * [new Mediator(eventEmitter, setting)](#new_MediatorDefinition.Mediator_new)
+  * _instance_
+    * [.addComponent(name, registrator)](#MediatorDefinition.Mediator+addComponent)
+    * [.removeComponent(name)](#MediatorDefinition.Mediator+removeComponent) ⇒ <code>boolean</code>
+    * [.hasComponent(name)](#MediatorDefinition.Mediator+hasComponent) ⇒ <code>boolean</code>
+    * [.getComponent(name)](#MediatorDefinition.Mediator+getComponent) ⇒ <code>Object</code>
+    * [.componentsList()](#MediatorDefinition.Mediator+componentsList) ⇒ <code>Array</code>
+    * [.shareResponsibility(mediator)](#MediatorDefinition.Mediator+shareResponsibility)
+    * [.toString()](#MediatorDefinition.Mediator+toString) ⇒ <code>string</code>
+  * _static_
+    * [.EVENTS](#MediatorDefinition.Mediator.EVENTS)
+      * [.ADD](#MediatorDefinition.Mediator.EVENTS.ADD)
+      * [.REMOVE](#MediatorDefinition.Mediator.EVENTS.REMOVE)
+
+<a name="new_MediatorDefinition.Mediator_new"></a>
+#### new Mediator(eventEmitter, setting)
+Mediator - Provide ability to you create a simple mediator to control interaction among components based on low coupling
+
+
+| Param | Type |
+| --- | --- |
+| eventEmitter | <code>EventEmitter</code> | 
+| setting | <code>Object</code> | 
+
+<a name="MediatorDefinition.Mediator+addComponent"></a>
+#### mediator.addComponent(name, registrator)
+Method provides ability to add component
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+
+| Param | Type |
+| --- | --- |
+| name | <code>string</code> | 
+| registrator | <code>function</code> | 
+
+<a name="MediatorDefinition.Mediator+removeComponent"></a>
+#### mediator.removeComponent(name) ⇒ <code>boolean</code>
+Method removes existing component, unattached event handlers for component and remove dependencies
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+**Returns**: <code>boolean</code> - True if component was removed in opposite false  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| name | <code>string</code> | Component Name |
+
+<a name="MediatorDefinition.Mediator+hasComponent"></a>
+#### mediator.hasComponent(name) ⇒ <code>boolean</code>
+Method provides you ability to check component existence by name
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+
+| Param |
+| --- |
+| name | 
+
+<a name="MediatorDefinition.Mediator+getComponent"></a>
+#### mediator.getComponent(name) ⇒ <code>Object</code>
+Method provides component by name
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+
+| Param |
+| --- |
+| name | 
+
+<a name="MediatorDefinition.Mediator+componentsList"></a>
+#### mediator.componentsList() ⇒ <code>Array</code>
+Method provides components' names list for all registered components
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+**Returns**: <code>Array</code> - Array that contains string names of each registered component  
+<a name="MediatorDefinition.Mediator+shareResponsibility"></a>
+#### mediator.shareResponsibility(mediator)
+Method can be used if you want to share events among several mediators in application
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+
+| Param | Type |
+| --- | --- |
+| mediator | <code>Mediator</code> | 
+
+<a name="MediatorDefinition.Mediator+toString"></a>
+#### mediator.toString() ⇒ <code>string</code>
+Return string for Instance of Mediator [object Mediator]
+
+**Kind**: instance method of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+<a name="MediatorDefinition.Mediator.EVENTS"></a>
+#### Mediator.EVENTS
+**Kind**: static constant of <code>[Mediator](#MediatorDefinition.Mediator)</code>  
+
+  * [.EVENTS](#MediatorDefinition.Mediator.EVENTS)
+    * [.ADD](#MediatorDefinition.Mediator.EVENTS.ADD)
+    * [.REMOVE](#MediatorDefinition.Mediator.EVENTS.REMOVE)
+
+<a name="MediatorDefinition.Mediator.EVENTS.ADD"></a>
+##### EVENTS.ADD
+Event with this name is emitted every time when new component is registered in system
+
+**Kind**: static constant of <code>[EVENTS](#MediatorDefinition.Mediator.EVENTS)</code>  
+<a name="MediatorDefinition.Mediator.EVENTS.REMOVE"></a>
+##### EVENTS.REMOVE
+Event with this name is emitted every time when component is removed in system
+
+**Kind**: static constant of <code>[EVENTS](#MediatorDefinition.Mediator.EVENTS)</code>  
--- mediator.js
+++ mediator.js
@@ -1,3 +1,4 @@
+/** @namespace MediatorDefinition */
 (function (global) {
   'use strict';
 
@@ -52,233 +53,244 @@
 
   /**
    * EventEmitter - provides event-driven system
+   * @class
    * @constructor
+   * @memberof MediatorDefinition
    */
   function EventEmitter() {
-    /**
-     * Dictionary for events
-     * @type {Object}
-     * @private
-     */
     this._events = {};
   }
 
-  _.extend(EventEmitter.prototype, /** @lends EventEmitter.prototype */{
+  _.extend(EventEmitter.prototype,
     /**
-     * Method provide ability to subscribe on some event by name and react on it by handler
-     * @method
-     * @param {string} eventName
-     * @param {function} handler
+     * @lends EventEmitter.prototype
+     * @memberof MediatorDefinition
      */
-    on: function (eventName, handler) {
-      if (this._events.hasOwnProperty(eventName)) {
-        this._events[eventName].push(handler);
-      } else {
-        this._events[eventName] = [handler];
-      }
-    },
-    /**
-     * Method allows to subscribe on some event and unsubscribe automatically after event will happen
-     * @method
-     * @param {string} eventName
-     * @param {function} handler
-     */
-    once: function (eventName, handler) {
-      var self = this;
-
-      self.on(eventName, decorator);
-
-      function decorator() {
-        try {
-          handler.apply(this, arguments);
-        } finally {
-          self.off(eventName, decorator);
+    {
+      /**
+       * Method provide ability to subscribe on some event by name and react on it by handler
+       * @method
+       * @param {string} eventName
+       * @param {function} handler
+       */
+      on: function (eventName, handler) {
+        if (this._events.hasOwnProperty(eventName)) {
+          this._events[eventName].push(handler);
+        } else {
+          this._events[eventName] = [handler];
         }
-      }
-    },
-    /**
-     * Method allows to remove subscription for specify handler of all event if handler is not defined
-     * @method
-     * @param {string} eventName
-     * @param {function} |handler|
-     */
-    off: function (eventName, handler) {
-      if (handler) {
-        var handlerIndex = this._events[eventName].indexOf(handler);
-        this._events[eventName].splice(handlerIndex, 1);
-      } else {
-        delete this._events[eventName];
-      }
-    },
-    /**
-     * Method allows to trigger all handler which are subscribed on some event and also pass any number of arguments
-     * @method
-     * @param eventName
-     */
-    emit: function (eventName) {
-      if (this._events.hasOwnProperty(eventName)) {
-        var args = Array.prototype.slice.call(arguments, 1);
+      },
+      /**
+       * Method allows to subscribe on some event and unsubscribe automatically after event will happen
+       * @method
+       * @param {string} eventName
+       * @param {function} handler
+       */
+      once: function (eventName, handler) {
+        var self = this;
 
-        this._events[eventName].forEach(function (handler) {
-          handler.apply(null, args);
-        });
+        self.on(eventName, decorator);
+
+        function decorator() {
+          try {
+            handler.apply(this, arguments);
+          } finally {
+            self.off(eventName, decorator);
+          }
+        }
+      },
+      /**
+       * Method allows to remove subscription for specify handler of all event if handler is not defined
+       * @method
+       * @param {string} eventName
+       * @param {function} |handler|
+       */
+      off: function (eventName, handler) {
+        if (handler) {
+          var handlerIndex = this._events[eventName].indexOf(handler);
+          this._events[eventName].splice(handlerIndex, 1);
+        } else {
+          delete this._events[eventName];
+        }
+      },
+      /**
+       * Method allows to trigger all handler which are subscribed on some event and also pass any number of arguments
+       * @method
+       * @param eventName
+       */
+      emit: function (eventName) {
+        if (this._events.hasOwnProperty(eventName)) {
+          var args = Array.prototype.slice.call(arguments, 1);
+
+          this._events[eventName].forEach(function (handler) {
+            handler.apply(null, args);
+          });
+        }
+      },
+      /**
+       * Overriding of standart toString method
+       * @method
+       * @returns {string} Returns string '[object EventEmitter]'
+       */
+      toString: function () {
+        return '[object EventEmitter]';
       }
-    },
-    /**
-     * Overriding of standart toString method
-     * @returns {string} Returns string '[object EventEmitter]'
-     */
-    toString: function () {
-      return '[object EventEmitter]';
-    }
-  });
+    });
 
   /**
    * Mediator - Provide ability to you create a simple mediator to control interaction among components based on low coupling
-   * @param eventEmitter
-   * @param setting
+   * @class
    * @constructor
+   * @param {EventEmitter} eventEmitter
+   * @param {Object} setting
+   * @memberof MediatorDefinition
    */
   function Mediator(eventEmitter, settings) {
     this._eventEmitter = eventEmitter;
-    /**
-     * Dictionary to store all registered components
-     * @type {Object}
-     * @private
-     */
     this._components = {};
-    /**
-     * Dictionary to store all event handler dependencies for registered components
-     * @type {Object}
-     * @private
-     */
     this._deps = {};
-
-    /**
-     * @type {Object}
-     * @private
-     */
     this._settings = settings;
   }
 
-  _.extend(Mediator, /** @lends Mediator */ {
-    EVENTS: {
-      ADD: 'mediator:add:component',
-      REMOVE: 'mediator:remove:component'
-    }
-  });
-
-  _.extend(Mediator.prototype, /** @lends Mediator.prototype */ {
+  _.extend(Mediator,
     /**
-     * Method gives ability to add component
-     * @method
-     * @param {string} name
-     * @param {Function} registrator
-     */
-    addComponent: function (name, registrator) {
-      if (this._components.hasOwnProperty(name)) {
-        throw new Error('Component [' + name + '] already exist')
+     * @lends Mediator
+     * @memberof MediatorDefinition
+     */ {
+      /**
+       * @constant
+       */
+      EVENTS: {
+        /**
+         * Event with this name is emitted every time when new component is registered in system
+         * @constant
+         */
+        ADD: 'mediator:add:component',
+        /**
+         * Event with this name is emitted every time when component is removed in system
+         * @constant
+         */
+        REMOVE: 'mediator:remove:component'
       }
+    });
 
-      var meta = registrator(function emit() {
-        if (this.hasComponent(name)) {
-          this._eventEmitter.emit.apply(this._eventEmitter, arguments);
-        } else {
-          throw new Error('Try to emit event by unregistered component: [' + name + ']');
+  _.extend(Mediator.prototype,
+    /**
+     * @lends Mediator.prototype
+     * @memberof MediatorDefinition
+     */
+    {
+      /**
+       * Method provides ability to add component
+       * @method
+       * @param {string} name
+       * @param {Function} registrator
+       */
+      addComponent: function (name, registrator) {
+        if (this._components.hasOwnProperty(name)) {
+          throw new Error('Component [' + name + '] already exist')
         }
-      }.bind(this));
 
-      if (typeof meta !== 'object') {
-        throw new Error('Component: [' + name + '] -> mediator#registrator function should return an object');
+        var meta = registrator(function emit() {
+          if (this.hasComponent(name)) {
+            this._eventEmitter.emit.apply(this._eventEmitter, arguments);
+          } else {
+            throw new Error('Try to emit event by unregistered component: [' + name + ']');
+          }
+        }.bind(this));
+
+        if (typeof meta !== 'object') {
+          throw new Error('Component: [' + name + '] -> mediator#registrator function should return an object');
+        }
+
+        if (meta.hasOwnProperty('onEvents')) {
+
+          this._deps[name] = [];
+
+          _.each(meta.onEvents, function (handler, eventName) {
+            this._eventEmitter.on(eventName, handler);
+            this._deps[name].push({
+              event: eventName,
+              handler: handler
+            });
+          }, this);
+
+          delete meta.onEvents;
+        }
+
+        this._components[name] = meta;
+        this._eventEmitter.emit(Mediator.EVENTS.ADD, name, meta);
+      },
+      /**
+       * Method removes existing component, unattached event handlers for component and remove dependencies
+       * @method
+       * @param {string} name Component Name
+       * @returns {boolean} True if component was removed in opposite false
+       */
+      removeComponent: function (name) {
+        if (this._deps.hasOwnProperty(name)) {
+          _.each(this._deps[name], function (dep) {
+            this.off(dep.event, dep.handler);
+          }, this._eventEmitter);
+
+          delete this._deps[name];
+        }
+
+        var componentDeleted = delete this._components[name];
+
+        if (componentDeleted) {
+          this._eventEmitter.emit(Mediator.EVENTS.REMOVE, name);
+        }
+
+        return componentDeleted;
+      },
+      /**
+       * Method provides you ability to check component existence by name
+       * @method
+       * @param name
+       * @returns {boolean}
+       */
+      hasComponent: function (name) {
+        return !!this._components[name];
+      },
+      /**
+       * Method provides component by name
+       * @method
+       * @param name
+       * @returns {Object}
+       */
+      getComponent: function (name) {
+        return this._components[name];
+      },
+      /**
+       * Method provides components' names list for all registered components
+       * @method
+       * @returns {Array} Array that contains string names of each registered component
+       */
+      componentsList: function () {
+        return Object.keys(this._components);
+      },
+      /**
+       * Method can be used if you want to share events among several mediators in application
+       * @method
+       * @param {Mediator} mediator
+       */
+      shareResponsibility: function (mediator) {
+        if (mediator instanceof Mediator) {
+          this._eventEmitter = mediator._eventEmitter;
+        }
+      },
+
+      /**
+       * Return string for Instance of Mediator [object Mediator]
+       * @method
+       * @returns {string}
+       */
+      toString: function () {
+        return '[object Mediator]';
       }
+    });
 
-      if (meta.hasOwnProperty('onEvents')) {
-
-        this._deps[name] = [];
-
-        _.each(meta.onEvents, function (handler, eventName) {
-          this._eventEmitter.on(eventName, handler);
-          this._deps[name].push({
-            event: eventName,
-            handler: handler
-          });
-        }, this);
-
-        delete meta.onEvents;
-      }
-
-      this._components[name] = meta;
-      this._eventEmitter.emit(Mediator.EVENTS.ADD, name, meta);
-    },
-    /**
-     * Method removes existing component, unattached event handlers for component and remove dependencies
-     * @method
-     * @param {string} name Component Name
-     * @returns {boolean} True if component was removed in opposite false
-     */
-    removeComponent: function (name) {
-      if (this._deps.hasOwnProperty(name)) {
-        _.each(this._deps[name], function (dep) {
-          this.off(dep.event, dep.handler);
-        }, this._eventEmitter);
-
-        delete this._deps[name];
-      }
-
-      var componentDeleted = delete this._components[name];
-
-      if (componentDeleted) {
-        this._eventEmitter.emit(Mediator.EVENTS.REMOVE, name);
-      }
-
-      return componentDeleted;
-    },
-    /**
-     * Method provides you ability to check component existence by name
-     * @method
-     * @param name
-     * @returns {boolean}
-     */
-    hasComponent: function (name) {
-      return !!this._components[name];
-    },
-    /**
-     *
-     * @param name
-     * @returns {Object}
-     */
-    getComponent: function (name) {
-      return this._components[name];
-    },
-    /**
-     * Method provides components' names list for all registered components
-     * @returns {Array} Array that contains string names of each registered component
-     */
-    componentsList: function () {
-      return Object.keys(this._components);
-    },
-    /**
-     *
-     * @param {Mediator} mediator
-     */
-    shareResponsibility: function (mediator) {
-      if (mediator instanceof Mediator) {
-        this._eventEmitter = mediator._eventEmitter;
-      }
-    },
-
-    /**
-     * Return string for Instance of Mediator [object Mediator]
-     * @returns {string}
-     */
-    toString: function () {
-      return '[object Mediator]';
-    }
-  });
-
-  // Export the Mediator object for **Node.js**, with
-  // backwards-compatibility for the old `require()` API. If we're in
-  // the browser, add `Mediator` as a global object.
   if (typeof exports !== 'undefined') {
     exports.EventEmitter = EventEmitter;
     exports.Mediator = Mediator;
